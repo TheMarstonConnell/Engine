2d451670f659b5a7ecd312f9dd977ec416310b8c
spencer <spencer@192.168.2.243>
2020-03-18 02:03:11 -0400

    trying to append items by editing a text file

--- src/Crafting/Items.java
+++ src/Crafting/Items.java
@@ -3,7 +3,7 @@ package Crafting;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.HashMap;
 import javax.swing.Timer;
 import roguelike_game.entity.EntityBase;
 import roguelike_game.entity.player.PlayerBase;
@@ -12,8 +12,23 @@ import xyz.marstonconnell.engine.render.Camera;
 import xyz.marstonconnell.engine.render.Sprite;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException; 
 
 public class Items {
-	Scanner in = new Scanner("items.txt");
-	
+	{
+	try {
+		HashMap<String,Integer> allItems = new HashMap<String,Integer>();
+		File itemsList = new File("items.txt");
+		Scanner readFile = new Scanner(itemsList);
+	} 	
+	catch (FileNotFoundException fe){
+		fe.printStackTrace();
+	}
+	/*FileReader fr = new FileReader("/Engine/assets/items.txt");
+	BufferedReader br = new BufferedReader(fr);
+    StringBuilder sb = new StringBuilder();
+*/}
 }
