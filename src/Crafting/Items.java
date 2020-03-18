e197b10dc453c9c916f4516323d02d0d4a588ede
spencer <spencer@192.168.2.243>
2020-03-17 21:10:14 -0400

    inventory still

--- src/Crafting/Items.java
+++ src/Crafting/Items.java
@@ -2,13 +2,18 @@ package Crafting;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.Timer;
 import roguelike_game.entity.EntityBase;
 import roguelike_game.entity.player.PlayerBase;
 import xyz.marstonconnell.engine.Engine;
 import xyz.marstonconnell.engine.render.Camera;
 import xyz.marstonconnell.engine.render.Sprite;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
-public class craft {
-	ArrayList<Items> items;
+public class Items {
+	Scanner in = new Scanner("items.txt");
+	
 }
--- src/Crafting/viewItems.java
+++
@@ -1,5 +0,0 @@
-package Crafting;
-
-public class viewItems {
-
-}
